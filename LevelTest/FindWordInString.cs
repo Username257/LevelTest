182d4fc4511c3c1c8deb211510b9c7891a569e76
Username257 <ddw4500@naver.com>
2023-04-14 19:56:08 +0900

    문자열 속에 문자 찾기

---
+++ LevelTest/FindWordInString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class FindWordInString
+    {
+        static void Main()
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            int index = str1.IndexOf(str2);
+            Console.Write(index);
+        }
+    }
+}
