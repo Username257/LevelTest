ca67adee054d185ca6c0f31caa7ad03479fff289
Username257 <ddw4500@naver.com>
2023-04-14 20:47:08 +0900

    feat: 문자열을 입력받으면 단어의 갯수를 출력하기
    띄어쓰기를 기준으로 단어를 셌다.
    반복문으로 문장의 문자를 하나하나 체크했다.

---
+++ LevelTest/InputStringPrintWordNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._04._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int wordsNum = 1;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ' ')
+                {
+                    wordsNum++;
+                }
+            }
+            Console.Write(wordsNum);
+        }
+    }
+}
+
