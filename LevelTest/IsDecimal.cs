1e63bdf85e9f31e6bb05f75af88e56a2fc08c543
Username257 <ddw4500@naver.com>
2023-04-16 18:11:00 +0900

    feat: Up & Down 게임 만들기
    기회를 10번으로 제한 하기 위해 카운트를 세고 if로 카운트가 10 이하일 때만 진행되도록 했다.
    10번의 기회 소진 시 재시작 여부 판단으로 Y를 입력받거나 그렇지 않은 것으로 했다.

---
+++ LevelTest/IsDecimal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class IsDecimal
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(IsPrime(n));
+        }
+        static bool IsPrime(int n)
+        {
+            if (n % 1 == 0 || n % 3 == 0 || n % 7 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
