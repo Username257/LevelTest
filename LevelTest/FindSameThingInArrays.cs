4672295cfbf16a220f136e3c4af28807020ce869
Username257 <ddw4500@naver.com>
2023-04-16 20:59:37 +0900

    feat: k개의 정렬된 배열에서 공통항목을 찾는 Solution을 완성하라.단, 중복은 허용하지 않는다.
    요소의 갯수가 유동적인 배열을 만들기 위해 List를 썼다.
    요소의 갯수는 입력으로 정해지며, 3개의 배열로 제한하였다.
    유동적인 배열을 위해 계속해서 리스트를 배열로 변환하였고, 중복 제거는 리스트의 Distinct를 이용했다.
    
    풀어보니 문제의 의도가 이게 아닌 거 같네

---
+++ LevelTest/FindSameThingInArrays.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class FindSameThingInArrays
+    {
+        public void Func()
+        {
+            int[] arr1 = { };
+            int[] arr2 = { };
+            int[] arr3 = {};
+            List<int> list = new List<int>();
+            int cnt = 0;
+            int cntArr = 1;
+            int num = 0;
+            string str;
+
+            while (true)
+            {
+                Console.Write($"배열 {cntArr}의 {cnt}번째 방 요소를 입력 해 주세요(그만하기 -> 문자, null 입력) : ");
+                str = Console.ReadLine();
+
+                if (int.TryParse(str, out num))
+                {
+                    list.Add(num);
+                    cnt++;
+                }
+                else
+                {
+                    cntArr++;
+                    cnt = 0;
+                }
+
+                if (cntArr == 2 && cnt == 0)
+                {
+                    Array.Resize(ref arr1, list.Count);
+                    arr1 = list.ToArray();
+                    list.Clear();
+                }
+
+                if (cntArr == 3 && cnt == 0)
+                {
+                    Array.Resize(ref arr2, list.Count);
+                    arr2 = list.ToArray();
+                    list.Clear();
+                }
+
+                if (cntArr == 4)
+                {
+                    Array.Resize(ref arr3, list.Count);
+                    arr3 = list.ToArray();
+                    break;
+                }
+            }
+            list.Clear();
+            list = FindCommonItems(arr1, arr2, arr3).ToList();
+            Console.WriteLine("결과 :");
+            foreach (int i in list)
+                Console.WriteLine(i);
+        }
+            
+
+        static int[] FindCommonItems(int[] arr1, int[] arr2, int[] arr3)
+        {
+            List<int> list = new List<int>();
+            int[] items = { };
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    for (int k = 0; k < arr3.Length; k++)
+                    {
+                        if (arr1[i] == arr2[j] && arr1[i] == arr3[k])
+                        {
+                            list.Add(arr1[i]);
+                        }
+                        
+                    }
+                }
+            }
+            list = list.Distinct().ToList();
+            Array.Resize(ref items, list.Count);
+            items = list.ToArray();
+            return items;
+        }
+
+        
+    }
+}
--- LevelTest/Program.cs
+++ LevelTest/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //문자열 속에 문자 찾기
+            Console.WriteLine("문자열 속에 문자 찾기");
+            FindWordInString FWIS = new FindWordInString();
+            FWIS.Func();
+
+            //문자열을 입력받으면 단어의 갯수를 출력하기
+            Console.WriteLine("\n\n문자열을 입력받으면 단어의 갯수를 출력하기");
+            InputStringPrintWordNum ISPWN = new InputStringPrintWordNum();
+            ISPWN.Func();
+
+            //주어진 숫자가 소수인지 판별하는 solution을 완성하라.
+            Console.WriteLine("\n\n주어진 숫자가 소수인지 판별하는 solution을 완성하라");
+            IsDecimal ID = new IsDecimal();
+            ID.Func();
+
+            //사용자가 입력한 양의 정수의 각 자리수의 합을 구하는 solution을 완성하라
+            Console.WriteLine("\n\n사용자가 입력한 양의 정수인 각 자리수의 합을 구하는 solution을 완성하라");
+            SumOfDigit SOD = new SumOfDigit();
+            SOD.Func();
+            
+            //k개의 정렬된 배열에서 공통항목을 찾는 Solution을 완성하라. 단, 중복X
+            Console.WriteLine("\n\nk개의 정렬된 배열에서 공통항목을 찾는 Solution을 완성하라. 단, 중복X");
+            FindSameThingInArrays FSTIA = new FindSameThingInArrays();
+            FSTIA.Func();
+            
+            //Up & Down 게임 만들기
+            Console.WriteLine("\n\nUp & Down 게임 만들기");
+            GameUpNDown GUND = new GameUpNDown();
+            GUND.Func();
+            
         }
     }
 }
