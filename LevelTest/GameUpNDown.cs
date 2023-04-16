ac400312a1dee77d300c5f89a1ca1a1ed9571c97
Username257 <ddw4500@naver.com>
2023-04-16 18:26:39 +0900

    feat: Up & Down 게임 만들기
    기회를 10번으로 제한 하기 위해 카운트를 세고 if로 카운트가 10 이하일 때만 진행되도록 했다.
    10번의 기회 소진 시 재시작 여부 판단으로 Y를 입력받거나 그렇지 않은 것으로 했다.

---
+++ LevelTest/GameUpNDown.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class GameUpNDown
+    {
+        public void Func()
+        {
+            Random randNum = new Random();
+            int com = randNum.Next(0, 10);
+            int my = 0;
+            int chance = 1;
+            string restart = "";
+
+            while (true)
+            {
+                Console.WriteLine($"{chance} 번째 입력");
+                Console.Write("수를 입력 해 주세요(0 ~ 999) : ");
+                my = int.Parse(Console.ReadLine());
+                if (chance < 10)
+                {
+                    chance++;
+                    if (com < my)
+                        Console.WriteLine("더 작습니다.");
+                    else if (com > my)
+                        Console.WriteLine("더 큽니다.");
+                    else
+                        break;
+                }
+                else
+                {
+                    Console.WriteLine("졌습니다. 재시작 하려면 Y를 입력 해 주세요.");
+                    restart = Console.ReadLine();
+                    if (restart != "Y")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        chance = 1;
+                    }
+                }
+
+
+            }
+            Console.WriteLine("게임이 종료되었습니다.");
+
+        }
+    }
+}
