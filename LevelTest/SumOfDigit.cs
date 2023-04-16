05808238d93fd6cf5870a2ff519f40452f422919
Username257 <ddw4500@naver.com>
2023-04-16 18:19:06 +0900

    feat: 사용자가 입력한 양의 정수의 각 자리수의 합을 구하는 solution을 완성하라
    주어진 함수의 원형은 매개변수로 int를 받았기 때문에 콘솔 입력시 string을 int로 변환했다.
    함수 내에서 입력받은 int를 다시 string으로 바꿔 문자들의 배열이라는 특징을 이용해 각 자릿수를 구했다.

---
+++ LevelTest/SumOfDigit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class SumOfDigit
+    {
+        public void Func()
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumOfDigits(num));
+            
+            static int SumOfDigits(int num)
+            {
+                string str = num.ToString();
+                int result = 0;
+                int temp = 0;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    temp = int.Parse(str[i].ToString());
+                    result += temp;
+                }
+                return result;
+            }
+
+        }
+    }
+}
